0290af6c7a677ec95403b325663d9f397a214dc7
Sungwook Kim <silenc3@naver.com>
2024-09-26 15:57:08 +0900

    feat: add Header blur

--- src/app/(Header)/Header.tsx
+++ src/app/(Header)/Header.tsx
@@ -2,7 +2,7 @@ import HeaderCategory from './HeaderCategory'
 
 function Header() {
   return (
-    <div className="fixed w-full bg-white">
+    <div className="fixed w-full bg-white bg-opacity-70 backdrop-blur-md">
       <div className="flex items-center justify-between px-8 py-4">
         <div className="flex items-center cursor-pointer w-30 h-12">
           SUNGWOOK
