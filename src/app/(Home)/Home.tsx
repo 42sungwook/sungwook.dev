888b2208b2c75cee9e2f911b7fcbb7309f4a5c65
Sungwook Kim <silenc3@naver.com>
2024-09-26 15:42:23 +0900

    feat: add home

--- src/app/(Home)/Home.tsx
+++ src/app/(Home)/Home.tsx
@@ -1,7 +1,25 @@
+import Link from 'next/link'
+
 function Home() {
   return (
-    <div className="flex items-center justify-center w-full h-screen">
-      <h1>HOME</h1>
+    <div className="flex items-center justify-center w-full h-screen bg-gradient-to-br from-gray-50 to-gray-100">
+      <div>
+        <h1 className="text-6xl">안녕하세요, 김성욱입니다</h1>
+        <div className="flex flex-col mt-10 text-center">
+          <p>
+            우연히 접한 42서울에서 다양한 사람들과 개발하면서 개발의 재미를
+            깨달았습니다
+          </p>
+          <p>주로 프론트엔드 개발을 하고 있으며 꾸준히 학습을 하고 있습니다</p>
+        </div>
+        <div className="mt-8 justify-center items-center flex">
+          <button className="bg-blue-500 text-white px-16 py-4 rounded-sm shadow-md hover:shadow-lg transition-all duration-300 ease-in-out hover:-translate-y-1">
+            <Link href="./#PROJECTS">
+              <p>프로젝트 보기</p>
+            </Link>
+          </button>
+        </div>
+      </div>
     </div>
   )
 }
