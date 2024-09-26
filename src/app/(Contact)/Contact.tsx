224139a4f678dfa0d0de03b09b5e887e3b8ef10e
Sungwook Kim <silenc3@naver.com>
2024-09-26 22:20:59 +0900

    feat: add Contact UI

--- src/app/(Contact)/Contact.tsx
+++ src/app/(Contact)/Contact.tsx
@@ -1,11 +1,21 @@
+import { Button, H1 } from '@/ui'
+
 function Contact() {
   return (
     <section
-      className="section"
-      id="CONTACT"
+      id="ABOUT"
+      className="section bg-gradient-to-br from-gray-50 via-gray to-gray-100"
     >
-      <div className="flex items-center justify-center w-full h-screen">
-        CONTACT
+      <div className="section-container">
+        <H1 className="mb-20 text-center">CONTACT</H1>
+        <div className="flex flex-col bg-white shadow-lg rounded-md gap-8 p-16 max-w-[80rem] w-[95%] mx-auto">
+          <div className="bg-gray-200 w-100 h-16 rounded-md"></div>
+          <div className="bg-gray-200 w-100 h-16 rounded-md"></div>
+          <div className="bg-gray-200 w-100 h-40 rounded-md"></div>
+          <div className="flex justify-end">
+            <Button>SUBMIT</Button>
+          </div>
+        </div>
       </div>
     </section>
   )
